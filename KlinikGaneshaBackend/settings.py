90b7bc2cdaa43ef18941794a4acc40e5e23da148
Haxorsprogramming <haxors@uinsu.ac.id>
2021-10-12 20:52:05 +0700

    'update'

--- KlinikGaneshaBackend/settings.py
+++ KlinikGaneshaBackend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kd4*!t522kqa0ar=&)7upwe#=4+*0@3)hy=q9e(+_w9+)mncbr'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'KlinikGaneshaBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER' : config('DB_USER'),
+        'PASSWORD' : config('DB_PASSWORD')
     }
 }
 
